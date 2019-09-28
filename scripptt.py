06dd05b0b2f3ff53f7a33c359b0e86bd2395bed3
Hamza Shakeel <47722357+hamzanufc10@users.noreply.github.com>
2019-09-28 17:18:40 +0530

    Update scripptt.py

--- scripptt.py
+++ scripptt.py
@@ -2,6 +2,8 @@ import requests
 import smtplib
 from bs4 import BeautifulSoup
 
+
+#whichever item you are planning to buy
 URL='https://www.amazon.in/Nivia-Storm-Football-Size-White/dp/B00ICCYF0E/ref=sr_1_1?dchild=1&keywords=fotball&qid=1569670655&sr=8-1'
 
 
@@ -30,8 +32,8 @@ def send_mail():
     server.ehlo()
     server.starttls()
     server.ehlo()
-
-    server.login('beinghamza@gmail.com','rjhiwrj')
+    #use 2-way authentication and genrete a password 
+    server.login('beinghamza@gmail.com','pa2dsasr432fsa33f32word123fsa324')
 
     subject='Hey!price fell down!'
     body='check the amzon link :https://www.amazon.in/Scotch-Brite-Bathroom-scrubber-brush-Green/dp/B00NBM24PS/ref=sr_1_13?keywords=brush&qid=1569183573&sr=8-13'
